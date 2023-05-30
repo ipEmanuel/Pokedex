c123895e69a10858f33e398bb6c3b7afa8b4b777
Emanuel <pei2688@gmail.com>
2023-05-29 23:37:14 -0300

    Se agregan card en la home y una pagina detalle

--- pokedex-web/Default.aspx.cs
+++ pokedex-web/Default.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using negocio;
+using dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,11 @@ namespace pokedex_web
 {
     public partial class Default : System.Web.UI.Page
     {
+        public List<Pokemon> ListaPokemon { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            PokemonNegocio negocio = new PokemonNegocio();
+            ListaPokemon = negocio.listarConSP();
         }
     }
 }
---
+++ pokedex-web/DetallePokemon.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="DetallePokemon.aspx.cs" Inherits="pokedex_web.DetallePokemon" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Detalle</h1>
+
+</asp:Content>
